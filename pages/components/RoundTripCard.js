0357db23d7634eb4c31fe4e65e498f6867960b9f
Melxy97 <861889629@qq.com>
2022-07-27 01:26:24 +0800

    feat: add basic components

---
+++ pages/components/RoundTripCard.js
@@ -0,0 +1,36 @@
+import BasicCard from "./BasicTripCard"
+export default function RoundCard(props) {
+    const {rId, segA, segB} = props
+    return (<div className="card" key={rId}>
+        <div className="state">{cTime} 变更({cType}) [{cState}]</div>
+        <div className={(cState === 'pending') ? "entity pending" : "entity"}>
+            <div className="flightNo">
+                {flightNo}
+            </div>
+            <div className="space-between">
+                <div className="date">
+                    {dDate}
+                </div>
+                <div className="date">
+                    {aDate}
+                </div>
+            </div>
+            <div className="space-between">
+                <div className="time changed">
+                    {dTime}
+                </div>
+                <div className="time changed">
+                    {aTime}
+                </div>
+            </div>
+            <div className="space-between">
+                <div className="city">
+                    {dCity}
+                </div>
+                <div className="city">
+                    {aCity}
+                </div>
+            </div>
+        </div>
+    </div>)
+}
